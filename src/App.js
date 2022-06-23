26c3f21e06b1f4af3a81eb5f824c71fe587d55e8
BoyThawatchai <thawatkw@ais.co.th>
2022-06-23 17:30:09 +0700

    change form

--- src/App.js
+++ src/App.js
@@ -2,12 +2,16 @@ import React, {useState} from 'react';
 import './App.css';
 
 function App() {
+
+  //State
   const [note, setNote] = useState({
     content: "",
     author: ""
   });
 
+  const [allNotes, setAllNotes] = useState([])
 
+  //FUnction
   function onNoteValueChange (event) {
     const {name, value} = event.target
     setNote((prevNote) => {
@@ -17,7 +21,25 @@ function App() {
     });
   } 
 
+  function onNoteSubmit(event) {
+    event.preventDefault()
 
+    setAllNotes((prevAllNotes) => {
+      return [...prevAllNotes, note]
+    })
+    
+  }
+
+  //Elements
+  const noteElements = allNotes.map((theNote, index) => {
+    return (
+      <div className='app-note' key={index}>
+      <p>{theNote.content}</p>
+      <h5>{theNote.author}</h5>
+    </div>
+    )
+    
+  })
 
   return (
     // <div className="App">
@@ -25,6 +47,7 @@ function App() {
     <section className='app-section'>
       <div className='app-container'>
         <h3> test</h3>
+        <form onSubmit={onNoteSubmit}>
         <p>
           <textarea rows={3} placeholder='บันทึกความในใจ' name="content" value={note.content} onChange={onNoteValueChange}
           />
@@ -33,9 +56,12 @@ function App() {
           <input type="text" placeholder='ลงชื่อ' name='author' value={note.author} onChange={onNoteValueChange}
           />
         </p>
-        
+        <p><button type='submit'>เพิ่มไปเลยสิค่ะที่รัก</button></p>
+        </form>
       </div>
-
+      <div className='app-notes'>
+        {noteElements}
+      </div>
     </section>
   );
 }
