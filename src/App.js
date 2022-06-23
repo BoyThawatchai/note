7ff2ca029d49a81dc6e95d9c9762f12201e974e1
BoyThawatchai <thawatkw@ais.co.th>
2022-06-23 13:47:26 +0700

    set usestate

--- src/App.js
+++ src/App.js
@@ -1,11 +1,25 @@
-import logo from './logo.svg';
+import React, {useState} from 'react';
 import './App.css';
 
 function App() {
+  const [noteConten, setNoteContent] = useState('');
+
+
   return (
-    <div className="App">
-      
-    </div>
+    // <div className="App">
+    // </div>
+    <section className='app-section'>
+      <div className='app-container'>
+        <h3> test</h3>
+        <p>
+          <input type="text" placeholder='บันทึกความในใจ' value={noteConten} onChange={(event) => {
+            setNoteContent(event.target.value)}}
+          />
+        </p>
+        
+      </div>
+
+    </section>
   );
 }
 
